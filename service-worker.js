5383c983e5c8715114743f6e773f9014e8ab1290
samkayem <samir.kayem@gmail.com>
2026-09-15 10:53:53 +0300

    Update CACHE_NAME to wissam-snack-v11

--- service-worker.js
+++ service-worker.js
@@ -46,7 +46,7 @@ self.addEventListener("notificationclick", (event) => {
   );
 });
 
-const CACHE_NAME = "wissam-snack-v10";
+const CACHE_NAME = "wissam-snack-v11";
 const ASSETS = [
   "./index.html",
   "./style.css",
